82e70adb5900877cdcdc71c6bb340cca4ddd9fbe
dhnascimento <danielhn@gmail.com>
2024-04-24 17:43:49 -0400

    refactor lemmatizer into class

--- lemmatizer.py
+++ lemmatizer.py
@@ -1,4 +1,3 @@
-
 import nltk
 from nltk.corpus import stopwords
 nltk.download('stopwords')
@@ -8,37 +7,36 @@ from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 import re
 import string
 
-STOPLIST = set(stopwords.words('english') + list(ENGLISH_STOP_WORDS))
+class LemmaTokenizer(object):
+    def __init__(self):
+        self.wnl = WordNetLemmatizer()
+        self.STOPLIST = set(stopwords.words('english') + list(ENGLISH_STOP_WORDS))
+        self.table = str.maketrans(dict.fromkeys(string.punctuation))  # OR {key: None for key in string.punctuation}
 
-def lemmaNLTK(sentence):
-    # Assigning lemmatizer to variable
-    lemmatizer = WordNetLemmatizer()
-    # Tokenizing the words
-    listofwords = re.split(" |'",sentence)
-    # List for storing the words after lowercasing and removing punctuation
-    listofwords2 = []
-    listoflemma_words = []
-    # List of punctuations
-    table = str.maketrans(dict.fromkeys(string.punctuation))  # OR {key: None for key in string.punctuation} 
-    
-    
-    for word in listofwords:
-        # Removing punctuaion
-        word_p = word.translate(table)
-        # Lower case
-        word_l = word_p.lower()
-        # Appending to list for next loop
-        listofwords2.append(word_l)
-    
-    for word_l in listofwords2:
-        # Ignore words in STOPLIST
-        if word_l in STOPLIST: continue      
-        # Lemmatizing the words
-        lemma_word = lemmatizer.lemmatize(word_l)
-        # Appending lemmatized words to list
-        listoflemma_words.append(lemma_word)
+    def __call__(self, sentence):
+        # Tokenizing the words
+        listofwords = re.split(" |'",sentence)
+        # List for storing the words after lowercasing and removing punctuation
+        listofwords2 = []
+        listoflemma_words = []
 
-    # Removing empty entries from the list    
-    listoflemma_words = list(filter(None, listoflemma_words))
-        
-    return listoflemma_words
+        for word in listofwords:
+            # Removing punctuation
+            word_p = word.translate(self.table)
+            # Lower case
+            word_l = word_p.lower()
+            # Appending to list for next loop
+            listofwords2.append(word_l)
+
+        for word_l in listofwords2:
+            # Ignore words in STOPLIST
+            if word_l in self.STOPLIST: continue
+            # Lemmatizing the words
+            lemma_word = self.wnl.lemmatize(word_l)
+            # Appending lemmatized words to list
+            listoflemma_words.append(lemma_word)
+
+        # Removing empty entries from the list
+        listoflemma_words = list(filter(None, listoflemma_words))
+
+        return listoflemma_words
